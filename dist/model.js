e854c29141c6669803ae6026181a08c06738eb99
dorcaslitunya <anonolitunya@gmail.com>
2022-08-23 22:53:37 +0300

    Interfaces, unions, enums and type of to give typeScript more power

--- dist/model.js
+++ dist/model.js
@@ -3,10 +3,23 @@ var inventoryType = "furniture";
 var trackingNumber = "FD123455";
 var createDate = new Date();
 var OriginalCost = 425;
+OriginalCost = 'this is so cool';
+var inventoryItemType;
+(function (inventoryItemType) {
+    inventoryItemType["Computer"] = "computer";
+    inventoryItemType["Furniture"] = "furniture";
+})(inventoryItemType || (inventoryItemType = {}));
+//you can give an inline description of the structure of the object
 function getInventoryItem(trackingNumber) {
+    return null;
 }
-function saveInventoryItem(trackingNumber) {
+function saveInventoryItem(item) {
 }
 var inventoryItem = getInventoryItem(trackingNumber);
+var updatedInventoryItem = inventoryItem;
 inventoryItem.createDate = new Date();
-saveInventoryItem(inventoryItem);
+saveInventoryItem({ displayName: "Dorc",
+    inventoryType: "computer",
+    trackingNumber: "123",
+    createDate: new Date(),
+    originalCost: 1500 });
--- src/model.ts
+++ src/model.ts
@@ -2,14 +2,24 @@ let displayName : string="Jess's standing desk";
 let inventoryType : string="furniture";
 let trackingNumber : string="FD123455";
 let createDate :Date =new Date();
-let OriginalCost : number =425;
+let OriginalCost : number | string =425;
+OriginalCost='this is so cool';
+
+enum inventoryItemType{
+    Computer='computer',
+    Furniture='furniture'
+}
+
 
 interface inventoryItem{
     displayName:string,
-    inventoryType:string,
+    inventoryType:"computer" | "furniture",
     trackingNumber:string,
     createDate:Date,
-    originalCost:number
+    originalCost?:number,
+
+    addNote?(note :string):string
+
 }
 //you can give an inline description of the structure of the object
 function getInventoryItem(trackingNumber:string): inventoryItem {
@@ -26,4 +36,8 @@ let updatedInventoryItem=inventoryItem;
 
 inventoryItem.createDate=new Date();
 
-saveInventoryItem(inventoryItem);
+saveInventoryItem({displayName:"Dorc",
+    inventoryType:"computer",
+    trackingNumber:"123",
+    createDate:new Date(),
+    originalCost:1500});
